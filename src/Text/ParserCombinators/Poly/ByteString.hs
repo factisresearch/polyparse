9a675033b61232df1b57376364ba43cf74769ff2
Malcolm.Wallace <Malcolm.Wallace@cs.york.ac.uk>
2007-03-28 16:55:38 +0000

    Abstract lots of commonality out of the separate concrete types.
    Previously, there was a lot of code duplication between the different
    variations of the various parsing monads (for state, for laziness, etc).
    
    In this re-organisation, we abstract the 'primitive' operations of each
    type into a class PolyParse, defined in Text.ParserCombinators.Poly.Base,
    together with many derived combinators which rely only on those primitives.
    
    Then each individual parser type is made an instance of the new class,
    each in its own module, defining just the primitives, and re-exporting
    the Poly.Base module.
    
    This re-organisation makes it very straightforward to provide ByteString
    and Stream versions of the Plain combinator set, where the input tokens
    are sequenced in non-list structures, and indeed might not even be
    polymorphic (as in ByteString).
    
    The original combinator modules have not been removed in this patch.  I
    still want to do some performance measurements to compare the overhead
    of the extra class mechanism.

---
+++ src/Text/ParserCombinators/Poly/ByteString.hs
@@ -0,0 +1,95 @@
+module Text.ParserCombinators.Poly.ByteString
+  ( -- * The Parser datatype
+    Parser(P)	-- datatype, instance of: Functor, Monad, PolyParse
+  , runParser	-- :: Parser a -> ByteString -> (Either String a, ByteString)
+    -- ** basic parsers
+  , next	-- :: Parser Char
+  , satisfy	-- :: (Char->Bool) -> Parser Char
+
+    -- ** re-parsing
+  , reparse	-- :: [Char] -> Parser ()
+    -- * Re-export all more general combinators
+  , module Text.ParserCombinators.Poly.Base
+  ) where
+
+
+import Text.ParserCombinators.PolyParse
+import Data.ByteString as BS
+
+-- | This @Parser@ datatype is a parsing monad with error reporting.
+--   It has fixed token type, namely Char stored in a ByteString, so
+--   cannot really be called 'poly' in the sense of polymorphic tokens.
+--   But in all other respects, this parser type follows the pattern,
+--   e.g. by having two levels of errors.
+newtype Parser a = P (ByteString -> (EitherE String a, ByteString))
+
+--   A return type like Either, that distinguishes not only between
+--   right and wrong answers, but also has gradations of wrongness.
+type EitherE a b = Either (Bool,a) b
+
+-- | Apply a parser to an input token sequence.
+runParser :: Parser a -> ByteString -> (Either String a, ByteString)
+runParser (P p) =
+    (\ (e,ts)-> (case e of {Left (_,m)->Left m; Right m->Right m}, ts) )
+    . p
+
+instance Functor Parser where
+    fmap f (P p) = P (\ts-> case p ts of
+                                (Left msg, ts') -> (Left msg,    ts')
+                                (Right x,  ts') -> (Right (f x), ts'))
+instance Monad Parser where
+    return x     = P (\ts-> (Right x, ts))
+    (P f) >>= g  = P (\ts-> case f ts of
+                                (Left msg, ts') -> (Left msg, ts')
+                                (Right x,  ts') -> let (P g') = g x in g' ts')
+    fail e       = P (\ts-> (Left (False,e), ts))
+
+instance PolyParse Parser where
+    failBad msg          = P (\ts-> (Left (True,msg), ts))
+    commit (P p)         = P (\ts-> case p ts of
+                                      (Left (_,e), ts') -> (Left (True,e), ts')
+                                      right             -> right )
+    (P p) `adjustErr` f  = P (\ts-> case p ts of
+                                 (Left (b,msg), ts') -> (Left (b,(f msg)), ts')
+                                 right               -> right )
+    (P p) `onFail` (P q) = P (\ts-> case p ts of
+                                      r@(Left (True,_), _) -> r
+                                      (Left _, _) -> q ts
+                                      right       -> right )
+    oneOf' = accum []
+      where accum errs [] =
+              case filter isBad errs of
+                [] ->  fail ("failed to parse any of the possible choices:\n"
+                            ++indent 2 (concatMap showErr (reverse errs)))
+                [(_,(_,e))] -> failBad e
+                es -> failBad ("one of the following failures occurred:\n"
+                              ++indent 2 (concatMap showErr (reverse es)))
+            accum errs ((e,P p):ps) =
+                P (\ts-> case p ts of
+                           (Left err,_) -> let (P p) = accum ((e,err):errs) ps
+                                           in p ts
+                           right        -> right )
+            showErr (name,(_,err)) = name++":\n"++indent 2 err
+            isBad (_,(b,_)) = b
+
+-- next token
+next :: Parser Char
+next = P (\ts-> case BS.uncons ts of
+                  Nothing  -> (Left (False,"Ran out of input (EOF)"), BS.empty)
+                  Just (t,ts') -> (Right (unsafeCoerce t), ts') )
+
+satisfy :: (Char->Bool) -> Parser Char
+satisfy p = do{ x <- next
+              ; if p x then return x else fail "Parse.satisfy: failed"
+              }
+
+------------------------------------------------------------------------
+-- | Push some tokens back onto the front of the input stream and reparse.
+--   This is useful e.g. for recursively expanding macros.  When the
+--   user-parser recognises a macro use, it can lookup the macro
+--   expansion from the parse state, lex it, and then stuff the
+--   lexed expansion back down into the parser.
+reparse    :: [Char] -> Parser ()
+reparse ts  = P (\inp-> (Right (), (BS.pack ts `BS.append` inp)))
+
+------------------------------------------------------------------------
---
+++ src/Text/ParserCombinators/Poly/Lazy.hs
@@ -0,0 +1,111 @@
+module Text.ParserCombinators.Poly.Lazy
+  ( -- * The Parser datatype.
+    -- $parser
+    Parser(P)	-- datatype, instance of: Functor, Monad
+  , runParser	-- :: Parser t a -> [t] -> (a, [t])
+    -- ** basic parsers
+  , next	-- :: Parser t t
+  , satisfy	-- :: (t->Bool) -> Parser t t
+    -- ** Re-parsing
+  , reparse	-- :: [t] -> Parser t ()
+    -- * Re-export all more general combinators
+  , module Text.ParserCombinators.Poly.Base
+  ) where
+
+import Text.ParserCombinators.Poly.Base
+
+#if __GLASGOW_HASKELL__
+import Control.Exception hiding (bracket)
+throwE :: String -> a
+throwE msg = throw (ErrorCall msg)
+#else
+throwE :: String -> a
+throwE msg = error msg
+#endif
+
+-- $parser
+-- When applied, these parsers do not return explicit failure.
+-- An exception is
+-- raised instead.  This allows partial results to be returned
+-- before a full parse is complete.
+-- One of the key ways to ensure that your parser is properly lazy,
+-- is to parse the initial portion of text returning a function, then
+-- use the @apply@ combinator to build the final value.
+
+-- | The @Parser@ datatype is a fairly generic parsing monad with error
+--   reporting.  It can be used for arbitrary token types, not just
+--   String input.  (If you require a running state, use module PolyStateLazy
+--   instead.)
+newtype Parser t a = P ([t] -> (Either String a, [t]))
+
+--   A return type like Either, that distinguishes not only between
+--   right and wrong answers, but also had gradations of wrongness.
+--   Not used in this library. !!!!!!!!!!!!!!!!!!!!!!!!!!!
+type EitherE a b = Either (Bool,a) b
+
+-- | Apply a parser to an input token sequence.  The parser cannot return
+--   an error value explicitly, so errors raise an exception.  Thus, results
+--   can be partial (lazily constructed, but containing undefined).
+runParser :: Parser t a -> [t] -> (a, [t])
+runParser (P p) =
+    (\ (e,ts)-> (case e of {Left m->throwE m; Right x->x}, ts) )
+    . p
+
+instance Functor (Parser t) where
+    fmap f (P p) = P (\ts-> case p ts of
+                                (Left msg, ts') -> (Left msg,    ts')
+                                (Right x,  ts') -> (Right (f x), ts'))
+instance Monad (Parser t) where
+    return x     = P (\ts-> (Right x, ts))
+    (P f) >>= g  = P (\ts-> case f ts of
+                                (Left msg, ts') -> (Left msg, ts')
+                                (Right x,  ts') -> let (P g') = g x in g' ts')
+    fail e       = P (\ts-> (Left e, ts))
+
+
+instance PolyParse (Parser t) where
+    failBad msg          = P (\ts-> (throwE msg, ts))
+    commit (P p)         = P (\ts-> case p ts of
+                                      (Left e, ts') -> (throwE e, ts')
+                                      right         -> right )
+    (P p) `onFail` (P q) = P (\ts-> case p ts of
+                                      (Left _, _) -> q ts
+                                      right       -> right )
+    (P p) `adjustErr` f  = P (\ts-> case p ts of
+                                      (Left msg, ts') -> (Left (f msg), ts')
+                                      right           -> right )
+    oneOf' ps = accum [] ps
+      where accum errs [] =
+              case errs of
+                [] ->  failBad ("internal failure in parser (oneOf'):\n"
+                               ++indent 2 (show (map fst ps)))
+                [(_,e)] -> fail e
+                es -> fail ("one of the following failures occurred:\n"
+                           ++indent 2 (concatMap showErr (reverse es)))
+            accum errs ((e,P p):ps) =
+                P (\ts-> case p ts of
+                           (Left err,_) -> let (P p) = accum ((e,err):errs) ps
+                                           in p ts
+                           right        -> right )
+            showErr (name,err) = name++":\n"++indent 2 err
+
+-- next token
+next = P (\ts-> case ts of
+                  []  -> (Left "Ran out of input (EOF)", [])
+                  (t:ts') -> (Right t, ts') )
+
+satisfy :: (t->Bool) -> Parser t t
+satisfy p = do{ x <- next
+              ; if p x then return x else fail "Parse.satisfy: failed"
+              }
+
+------------------------------------------------------------------------
+-- | Push some tokens back onto the front of the input stream and reparse.
+--   This is useful e.g. for recursively expanding macros.  When the
+--   user-parser recognises a macro use, it can lookup the macro
+--   expansion from the parse state, lex it, and then stuff the
+--   lexed expansion back down into the parser.
+reparse    :: [t] -> Parser t ()
+reparse ts  = P (\inp-> (Right (), ts++inp))
+
+------------------------------------------------------------------------

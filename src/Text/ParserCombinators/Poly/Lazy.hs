9676dc926acd944c7a708d568301a0df6d9e2de4
Malcolm.Wallace <Malcolm.Wallace@me.com>
2010-10-31 18:33:28 +0000

    Workaround to allow different instances Applicative for Parser type.

--- src/Text/ParserCombinators/Poly/Lazy.hs
+++ src/Text/ParserCombinators/Poly/Lazy.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP, GeneralizedNewtypeDeriving #-}
 module Text.ParserCombinators.Poly.Lazy
   ( -- * The Parser datatype
     Parser(P)	-- datatype, instance of: Functor, Monad, PolyParse
@@ -17,7 +18,7 @@ module Text.ParserCombinators.Poly.Lazy
 
 import Text.ParserCombinators.Poly.Base
 import Text.ParserCombinators.Poly.Result
-import Text.ParserCombinators.Poly.Parser
+import qualified Text.ParserCombinators.Poly.Parser as P
 import Control.Applicative
 
 #if __GLASGOW_HASKELL__
@@ -29,12 +30,25 @@ throwE :: String -> a
 throwE msg = error msg
 #endif
 
--- The only differences between a Plain and a Lazy parser are the instance
--- of Applicative, and the type (and implementation) of runParser.
+-- | The only differences between a Plain and a Lazy parser are the instance
+--   of Applicative, and the type (and implementation) of runParser.
+--   We therefore need to /newtype/ the original Parser type, to allow it
+--   to have a different instance.
+newtype Parser t a = P (P.Parser t a)
+#ifdef __GLASGOW_HASKELL__
+        deriving (Functor,Monad,Commitment)
+#else
+instance Functor (Parser t) where
+    fmap f (P (P.P p)) = P (P.P . fmap f . p)
+instance Monad (Parser t) where
+    return x  = P (return x)
+    fail e    = P (fail e)
+    (P f) >>= (P g) = P (f >>= g)
+#endif
 
 -- | Apply a parser to an input token sequence.
 runParser :: Parser t a -> [t] -> (a, [t])
-runParser (P p) = fromResult . p
+runParser (P (P.P p)) = fromResult . p
   where
     fromResult :: Result z a -> (a, z)
     fromResult (Success z a)  =  (a, z)
@@ -47,7 +61,7 @@ instance Applicative (Parser t) where
     --   is strict in the result of the function parser, but
     --   lazy in the result of the argument parser.  (Argument laziness is
     --   the distinctive feature over other implementations.)
-    (P pf) <*> px = P (continue . pf)
+    (P (P.P pf)) <*> px = P (P.P (continue . pf))
       where
         continue (Success z f)  = let (x,z') = runParser px z
                                   in Success z' (f x)
@@ -58,9 +72,24 @@ instance Applicative (Parser t) where
 #endif
 
 instance Alternative (Parser t) where
-    empty     = fail "no parse"
-    p <|> q   = p `onFail` q
+    empty             = fail "no parse"
+    (P p) <|> (P q)   = P (p `P.onFail` q)
 
 instance PolyParse (Parser t)
 
+------------------------------------------------------------------------
+next    ::  Parser t t
+next    = P P.next
+
+eof     :: Parser t ()
+eof     = P P.eof
+
+satisfy :: (t->Bool) -> Parser t t
+satisfy = P . P.satisfy
+
+onFail  :: Parser t a -> Parser t a -> Parser t a
+onFail (P a) (P b) = P (a `P.onFail` b)
+
+reparse :: [t] -> Parser t ()
+reparse = P . P.reparse
 ------------------------------------------------------------------------
